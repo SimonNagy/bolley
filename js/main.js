d10be3cde85aadc00c9f77820012ae06a15aa577
SimonNagy <nagy.simon@kjk.bme.hu>
2022-04-01 15:57:08 +0200

    dinamikus navbar
    JS-main.js

--- js/main.js
+++ js/main.js
@@ -155,3 +155,23 @@ $(function() {
 	contactForm();
 
 });
+
+// dinamikus navbar
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
